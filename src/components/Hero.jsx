9d8ff8b669608eddd2380465600c01a55b796cc8
shivamkumar18 <mahurshivamkumar@gmail.com>
2026-08-22 14:21:21 +0530

    little change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Navbar from './Navbar';
-import github from '/github.png';
-import linkedin from '/linkedin.png';
-import facebook from '/facebook.png';
-import instagram from '/instagram.png';
-import ShivamKumar from '/ShivamKumar_InternshalaResume.pdf';
+import github from '/github.png'
+import linkedin from '/linkedin.png'
+import leetcode from '/leetcode.png'
+import instagram from '/instagram.png'
+import shivamkumar from '/shivam_kumar_resume_.pdf'
 import spid from '/spid.png';
 import { motion } from 'framer-motion';
 
@@ -29,32 +29,32 @@ export default function Hero() {
                 >
                     <div className='absolute -z-10 w-60 h-60 bg-[#800000] rounded-full blur-3xl opacity-55 -top-5 -left-12  '></div>
                     <header>
-                        <h1 className='text-4xl sm:text-4xl md:text-5xl font-bold text-white mb-4 '>
+                        {/* <h1 className='text-4xl sm:text-4xl md:text-5xl font-bold text-white mb-4 '>
                             Shivam kumar
-                        </h1>
+                        </h1> */}
                         <h2 className='text-xl sm:text-4xl md:text-2xl font-bold text-[#3e0f4a] md:text-[#05a72b] mb-2 ' >
-                            Frontend Developer
+                            Full Stcak Devloper
                         </h2>
                     </header>
                     <p className='text-base sm:text-lg text-gray-200 mb-6'>
-                        Passionate about crafting responsive, visually engaging, and user-friendly web experiences.
+                         Passionate about transforming ideas into reliable, scalable, and impactful digital solutions.
                     </p>
 
                     <div className='flex items-center space-x-4 mb-6 '>
-                        <a href="#">
-                            <img src={github} alt="github" className='w-11 h-11' />
+                        <a href="https://github.com/Shiamkumar18">
+                            <img src={github}  alt="github"  className='w-11 h-11 hover:scale-110 transition-transform duration-400' />
                         </a>
-                        <a href="#">
-                            <img src={linkedin} alt="Linkedin" className='w-11 h-11' />
+                        <a href="https://www.linkedin.com/in/shivam-kumar-19a000320/ ">
+                            <img src={linkedin} alt="Linkedin" className='w-11 h-11 hover:scale-110 transition-transform duration-400' />
                         </a>
-                        <a href="#">
-                            <img src={instagram} alt="instagram" className='w-11 h-11' />
+                        <a href="https://leetcode.com/u/Shivam_kumar_18/">
+                            <img src={leetcode} alt="leetcode" className='w-10 h-10 hover:scale-110 transition-transform duration-300' />
                         </a>
-                        <a href="#">
-                            <img src={facebook} alt="facebook" className='w-11 h-11' />
-                        </a>
+                        {/* <a href="">
+                            <img src={instagram} alt="facebook" className='w-11 h-11' />
+                        </a> */}
                     </div>
-                    <a href={ShivamKumar} download>
+                    <a href={shivamkumar} download>
                         <button className='inline-flex text-white border-2 py-2 px-6 focus:outline-none hover:bg-[#F4D331]
                                  hover:shadow-[0_0_40px_rgba(128,0,128,0.7)] rounded-full text-lg '>
                             Download CV
